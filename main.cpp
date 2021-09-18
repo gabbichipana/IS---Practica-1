500a1fd61417340bc60357a5e54c557d2987ea18
gabbichipana <gchipanap@unsa.edu.pe>
2021-09-17 23:05:49 -0500

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/* Gabriela Angel Chipana Perez
+	20192268
+	Estudiante de Ciencia de la Computacion 
+	Universidad Nacional de San Agustin de Arequipa
+*/ 
+
+struct Point
+{
+	int x;
+	int y;
+};
+bool isAdjacent(Point a, Point b)
+{
+	if(a.x == b.x || a.y == b.y || a.x == b.y || a.y == b.x)
+		return true;
+	else 
+		return false;
+}
+void infect(Point a)
+{
+	if(isAdjacent(a, ))
+}
+void mapIsla(int ** matrix, int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		for(int j = 0 ; j < size; j++)
+		{
+			if()
+		}
+	}
+}
